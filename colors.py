43030f75d2c96f41074c4eae8102fd6a141ff90e
Kozova1 <mug66kk@gmail.com>
2020-05-13 11:13:26 +0300

    Formatted python files with black

--- colors.py
+++ colors.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 """Prints the 16 basic terminal colors"""
 import os
+
 COLORS = range(15)
-BASE_CMD = 'tput setab '
-print('|', end='', flush=True)
+BASE_CMD = "tput setab "
+print("|", end="", flush=True)
 for c in COLORS:
     os.system(BASE_CMD + str(c) + '; printf "  "; tput sgr0; printf "|"')
-    os.system('tput sgr0')
+    os.system("tput sgr0")
 print()
-print('|', end='')
+print("|", end="")
 for c in COLORS:
     if len(str(c)) > 1:
-        print(f'{c}|', end='')
+        print(f"{c}|", end="")
     else:
-        print(f' {c}|', end='')
+        print(f" {c}|", end="")
 print()
--- segments.py
+++ segments.py
@@ -1,89 +1,107 @@
 #!/usr/bin/env python3
 """Creates a PS1 from a prompt.json file."""
 
-import sys
-import os
 import json
-from typing import List, Dict
+import os
+import sys
+from typing import Dict, List
 
-class Color():
+
+class Color:
     """This class stores a terminal color - foreground and background are each 0-255"""
+
     def __init__(self, foreground: int, background: int):
         self.foreground: int = foreground
         self.background: int = background
+
     def get_background(self) -> int:
         """Returns the background color"""
         return self.background
+
     def get_foreground(self) -> int:
         """Returns the foreground color"""
         return self.foreground
+
 
 def swap(color: Color):
     """Returns a color with the background and foreground swapped"""
     return Color(color.get_background(), color.get_foreground())
 
+
 def setcolor(color: Color) -> str:
     """Generate tput command to set colors"""
     bg_col = color.get_background()
     fg_col = color.get_foreground()
-    return f'\\[$(tput sgr0; tput setaf {fg_col}; tput setab {bg_col})\\]'
+    return f"\\[$(tput sgr0; tput setaf {fg_col}; tput setab {bg_col})\\]"
+
 
 def set_sepcolor(color: Color) -> str:
     """Sets the color of the separator"""
     return setcolor(swap(color) if sep_inverse else color)
 
-class Segment():
+
+class Segment:
     """This class represents a single segment in the prompt"""
+
     def __init__(self, bgcol: int, fgcol: int, contents: str):
         self.color: Color = Color(fgcol, bgcol)
         self.contents: str = contents
+
     def draw(self) -> str:
         """Renders the Segment into bash syntax"""
         my_color = self.get_color()
         my_contents = self.get_contents()
         return setcolor(my_color) + my_contents
+
     def get_contents(self) -> str:
         """Returns the contents of the segment"""
         return self.contents
+
     def get_color(self) -> Color:
         """Returns the color of the segment"""
         return self.color
 
+
 prompt_obj: Dict[str, any] = {}
 try:
-    s: str = ''
-    base_conf_dir = os.getenv('XDG_CONFIG_HOME', '~/.config')
-    file_path = os.path.join(base_conf_dir, 'bash-segments', 'prompt.json')
-    with open(os.path.expanduser(file_path), 'r') as f:
+    s: str = ""
+    base_conf_dir = os.getenv("XDG_CONFIG_HOME", "~/.config")
+    file_path = os.path.join(base_conf_dir, "bash-segments", "prompt.json")
+    with open(os.path.expanduser(file_path), "r") as f:
         s = f.read()
     prompt_obj = json.loads(s)
 except ValueError:
-    print('JSON Decode error, verify that your prompt.json is valid json', file=sys.stderr)
+    print(
+        "JSON Decode error, verify that your prompt.json is valid json", file=sys.stderr
+    )
     sys.exit(1)
 except OSError:
-    print('File open error, verify that prompt.json exists and is readable', file=sys.stderr)
+    print(
+        "File open error, verify that prompt.json exists and is readable",
+        file=sys.stderr,
+    )
     sys.exit(1)
 
 segments: List[Segment] = []
 obj_segs: List[any] = []
-separator: str = '|'
+separator: str = "|"
 sep_inverse: bool = False
 IS_SPACE_END: bool = True
 BCOLOR: int = 0
 try:
-    obj_segs = prompt_obj['segments']
-    separator = prompt_obj['separator']
-    sep_inverse = prompt_obj['separator_inverse']
-    IS_SPACE_END = prompt_obj['space_at_end']
-    BCOLOR = prompt_obj['background_color']
+    obj_segs = prompt_obj["segments"]
+    separator = prompt_obj["separator"]
+    sep_inverse = prompt_obj["separator_inverse"]
+    IS_SPACE_END = prompt_obj["space_at_end"]
+    BCOLOR = prompt_obj["background_color"]
 except KeyError:
     pass
 for seg in obj_segs:
     bg: int = 0
     fg: int = 1
-    content: str = '?'
+    content: str = "?"
     try:
-        bg, fg, content = seg['background'], seg['foreground'], seg['content']
+        bg, fg, content = seg["background"], seg["foreground"], seg["content"]
     except KeyError:
         pass
     segments.append(Segment(bg, fg, content))
@@ -103,9 +121,9 @@ for i, seg in enumerate(segments):
         this_bg = seg.get_color().get_background()
         next_bg = next_seg.get_color().get_background()
         new_color: Color = Color(this_bg, next_bg)
-    final_prompt += (set_sepcolor(new_color) + separator)
+    final_prompt += set_sepcolor(new_color) + separator
 
 final_prompt += '$(tput sgr0)"'
 if IS_SPACE_END:
-    final_prompt += ' '
+    final_prompt += " "
 print(final_prompt)
